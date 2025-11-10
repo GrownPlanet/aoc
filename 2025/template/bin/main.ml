116d4678d520844dfb28876b11a1de116b59a32a
Timo dh <timo.dehandschutter@gmail.com>
2025-11-10 21:42:02 +0100

    update template for ocaml

--- 2025/template/bin/main.ml
+++
@@ -1,9 +0,0 @@
-open Aoc
-
-let () =
-  print_endline "part 1:";
-  P1.solve ()
-  (*
-  print_endline "part 2:";
-  P2.solve ();
-  *)
--- 2025/template/lib/p2.ml
+++
@@ -1,2 +0,0 @@
-let solve () =
-  print_endline "not implemented yet"
